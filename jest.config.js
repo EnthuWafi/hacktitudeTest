f3956e7ac0583c8f2acd15b92621ce0e6487c055
EnthuWafi <108134135+EnthuWafi@users.noreply.github.com>
2023-07-29 15:24:37 +0800

    adding .gitignore file

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,7 @@ export default async () => {
     Replace the <INSERT YOUR FILE NAME HERE> with the name of the cloned repo folder name 
     Eg: "../23da1134-b7a2-4e2f-bcdf-59dbab5a7595"
   */
-  let __dirname = "../<INSERT YOUR FILE NAME HERE>";
+  let __dirname = "../7d5a6c2a-f747-4db6-aa49-f221ec08e737";
   return {
     rootDir: resolve(__dirname, ''),
     verbose: true,
